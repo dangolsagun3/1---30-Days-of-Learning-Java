8449af279de09d77768f966ae8285c3bb64b0e14
SAGUN DANGOL <dangolsagun6@gmail.com>
2026-03-11 18:53:34 +0545

    finding leapyear java programming added to 30 days learning java

---
+++ leapyear.java
@@ -0,0 +1,9 @@
+public class leapyear {
+    public static void main(String[] args) {
+        int year = 2020;
+        if (year % 4 == 0)
+            System.out.println("Leap Year");
+        else
+            System.out.println("Not Leap Year");
+    }
+}
