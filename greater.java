1da63d484083d0e571d41ec8bfb0cd42ef7cfe1c
SAGUN DANGOL <dangolsagun6@gmail.com>
2026-03-11 18:46:16 +0545

    greater and greatest java programming added

---
+++ greater.java
@@ -0,0 +1,11 @@
+public class greater {
+    public static void main(String[] args) {
+        int a = 100, b = 15;
+        if(a>b){
+            System.out.println("A is Greater");
+        }
+        else{
+            System.out.println("B is Greater");
+        }
+    }
+}
---
+++ greatest.java
@@ -0,0 +1,14 @@
+public class greatest {
+    public static void main(String[] args){
+        int a = 100, b = 20, c =350;
+        if (a>b && a>c){
+            System.out.println("A is Greater");
+        }
+        else if (b>c && b>a){
+            System.out.println("B is Greater");
+        }
+        else{
+            System.out.println("C is Greater");
+        }
+    }
+}
