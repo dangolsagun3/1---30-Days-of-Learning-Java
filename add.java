502e8b9d142e500e6132f37cd9392cb9ed20e8e6
SAGUN DANGOL <dangolsagun6@gmail.com>
2026-03-11 18:45:00 +0545

    simple add java programming added

---
+++ add.java
@@ -0,0 +1,14 @@
+// constructor
+
+public class add {
+        add() {
+        int a = 10, b = 20, sum;
+         sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+    public static void main (String[] args)
+    {
+        add a1 = new add();
+
+    }
+}
