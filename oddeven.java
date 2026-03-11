61d0f47a6efbda1a4fffbdf2b3fb50d5f73a5625
SAGUN DANGOL <dangolsagun6@gmail.com>
2026-03-11 18:51:55 +0545

    odd or even java programming added to 30 days learning java

---
+++ oddeven.java
@@ -0,0 +1,12 @@
+public class oddeven {
+    public static void main(String[] args){
+        int a = 2;
+        if (a%2==0){
+            System.out.println("Even");
+        }
+        else {
+            System.out.println("Odd");
+        }
+    }
+    
+}
